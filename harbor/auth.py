3e533addae6d146bc5ac3e0428b8cf41afd1e3c4
David Cooper <david@dtcooper.com>
2020-08-24 19:53:51 -0700

    Update auth script to new spreadsheet format

--- harbor/auth.py
+++ harbor/auth.py
@@ -13,17 +13,19 @@ import argparse
 from collections import OrderedDict
 import datetime
 import json
+import pickle
+import pprint
 import os
 import sys
 
-from dateutil.parser import parse as dateutil_parse
+from dateutil.parser import parse as dateutil_parse, ParserError
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import pytz
 
 
 SHEET_TIMEZONE = pytz.timezone('US/Pacific')
-AUTH_DATA_FILE = os.path.join(os.path.dirname(__file__), 'auth-data.json')
+USER_DATA_CACHE_FILE = os.path.join(os.path.dirname(__file__), 'user-data.pkl')
 CREDENTIALS_FILE = os.path.join(os.path.dirname(__file__), 'credentials.json')
 if not os.path.exists(CREDENTIALS_FILE):
     print(f'No Google credentials file found at {CREDENTIALS_FILE}. Exiting.')
@@ -34,52 +36,56 @@ def sanitize_password(password):
     return password.lower().strip()
 
 
-def cache_auth_data(sheet_key):
+def get_and_cache_user_data(sheet_key):
     creds = ServiceAccountCredentials.from_json_keyfile_name(CREDENTIALS_FILE, [
         'https://spreadsheets.google.com/feeds',
         'https://www.googleapis.com/auth/calendar.readonly',
         'https://www.googleapis.com/auth/drive',
     ])
     client = gspread.authorize(creds)
-    auth_data = OrderedDict()
+    data = OrderedDict()
 
-    sheet = client.open_by_key(sheet_key)
-    schedule, passwords = sheet.worksheets()
-    schedule_rows = schedule.get_all_values()
-    password_rows = passwords.get_all_values()
+    sheet = client.open_by_key(sheet_key).sheet1
+    rows = sheet.get_all_values()
 
-    assert schedule_rows[1] == ['DJ Name', 'Show Start (Pacific Time)', 'Show End (Pacific Time)']
-    assert password_rows[0] == ['DJ Name', 'Password', 'Email']
-    for (dj_name_schedule, start, end), (dj_name_password, password, _) in zip(
-        schedule_rows[2:], password_rows[1:]
-    ):
-        assert dj_name_schedule == dj_name_password
-        auth_data[sanitize_password(password)] = (dj_name_schedule, start, end)
+    headers = ('name', 'start', 'end', 'password')
+    for header_row, row in enumerate(rows):
+        if all(header in row[column].lower() for column, header in enumerate(headers)):
+            break
+    else:
+        print("Couldn't find header row.")
+        sys.exit(1)
 
-    with open(AUTH_DATA_FILE, 'w') as auth_file:
-        json.dump(auth_data, auth_file, indent=2)
+    for name, start, end, password, *_ in rows[header_row + 1:]:
+        try:
+            start = SHEET_TIMEZONE.localize(dateutil_parse(start))
+            end = SHEET_TIMEZONE.localize(dateutil_parse(end))
+        except ParserError:
+            continue
 
-    return auth_data
+        password = sanitize_password(password)
+        if password:
+            data[password] = OrderedDict((('name', name or 'unknown'), ('start', start), ('end', end)))
+
+    with open(USER_DATA_CACHE_FILE, 'wb') as cache_file:
+        pickle.dump(data, cache_file)
+
+    return data
 
 
-def authorize_data(auth_data, password, date, grace=None):
-    data = {
-        'authorized': False,
-        'valid_user': False,
-    }
+def auth_data(user_data, password, date, grace=0):
+    data = OrderedDict((('authorized', False), ('valid_user', False)))
 
-    user_data = auth_data.get(sanitize_password(password))
+    user_data = user_data.get(sanitize_password(password))
     if user_data is not None:
-        dj_name, start, end = user_data
-        data.update({'valid_user': True, 'name': dj_name, 'start': start, 'end': end})
-
-        start, end = map(lambda s: SHEET_TIMEZONE.localize(dateutil_parse(s)), (start, end))
-        if grace is not None:
-            start -= datetime.timedelta(seconds=grace)
-            end += datetime.timedelta(seconds=grace)
-
+        data['valid_user'] = True
+        for key, value in user_data.items():
+            data[key] = str(value)
+        start = user_data['start'] - datetime.timedelta(seconds=grace)
+        end = user_data['end'] + datetime.timedelta(seconds=grace)
+        data['start_with_grace_period'], data['end_with_grace_period'] = str(start), str(end)
         data['authorized'] = start <= date <= end
-        data['seconds_to_kick'] = round((end - date).total_seconds()) if data['authorized'] else -1
+        data['seconds_to_kickoff'] = round((end - date).total_seconds()) if data['authorized'] else -1
 
     return data
 
@@ -92,24 +98,27 @@ def main():
     parser.add_argument('-d', '--date', help='date and time to check (default: now)')
     parser.add_argument('-f', '--force', help="force a download when checking password (don't use cache)")
     parser.add_argument('-g', '--grace-period', type=int, default=90, help='grace period in seconds (default: 90)')
+    parser.add_argument('--dump', action='store_true', help='dump all user data')
 
     args = parser.parse_args()
 
-    if args.force or not args.password or not os.path.exists(AUTH_DATA_FILE):
-        auth_data = cache_auth_data(sheet_key=args.sheet_key)
+    if args.force or not args.password or not os.path.exists(USER_DATA_CACHE_FILE):
+        user_data = get_and_cache_user_data(sheet_key=args.sheet_key)
     else:
-        with open(AUTH_DATA_FILE, 'r') as auth_file:
-            auth_data = json.load(auth_file)
+        with open(USER_DATA_CACHE_FILE, 'rb') as cache_file:
+            user_data = pickle.load(cache_file)
 
-    if args.password:
+    if args.dump:
+        pprint.pprint(user_data)
+    elif args.password:
         timezone = pytz.timezone(args.timezone)
         if args.date:
             date = timezone.localize(dateutil_parse(args.date, fuzzy=True).replace(tzinfo=None))
         else:
             date = datetime.datetime.now(timezone)
 
-        data = authorize_data(auth_data=auth_data, password=args.password, date=date, grace=args.grace_period)
-        print(json.dumps(data, indent=2, sort_keys=True))
+        data = auth_data(user_data=user_data, password=args.password, date=date, grace=args.grace_period)
+        print(json.dumps(data, indent=2))
 
 
 if __name__ == '__main__':
