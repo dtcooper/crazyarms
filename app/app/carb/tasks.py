72a5a5d6362d9d2fc6d63f969db7ff0fc8b07575
David Cooper <david@dtcooper.com>
2020-10-14 23:32:33 -0700

    Basic telnet requests

--- app/app/carb/tasks.py
+++ app/app/carb/tasks.py
@@ -7,6 +7,7 @@ import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
+from .liquidsoap import Liquidsoap  # noqa: E402
 from .models import ScheduledBroadcast, PLAY_STATUS_PLAYED  # noqa: E402
 
 
@@ -18,7 +19,9 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 def play_scheduled_broadcast(scheduled_broadcast_id):
     try:
         scheduled_broadcast = ScheduledBroadcast.objects.get(id=scheduled_broadcast_id)
-        print(f'Would play: {scheduled_broadcast.asset_path}')
+        # TODO: have a thread local / open telnet per worker
+        liquidsoap = Liquidsoap()
+        liquidsoap.request__push(scheduled_broadcast.asset_path)
         ScheduledBroadcast.objects.filter(id=scheduled_broadcast_id).update(play_status=PLAY_STATUS_PLAYED)
     except ScheduledBroadcast.DoesNotExist:
         print(f'Scheduled broadcast does not exist [id={scheduled_broadcast_id}]')
