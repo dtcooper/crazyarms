00ea2634d1d89bf180b82f74de9af84142ec3e25
David Cooper <david@dtcooper.com>
2021-03-31 12:47:34 -0400

    Fixed #4 and better upstream logging

--- app/app/services/templatetags/services.py
+++ app/app/services/templatetags/services.py
@@ -7,21 +7,26 @@ register = template.Library()
 
 @register.filter
 def liqval(value, comment_string=True):
-    if isinstance(value, str):
+    if isinstance(value, bool):
+        encoded = str(value).lower()
+
+    elif isinstance(value, float):
+        encoded = f"{value:.5g}"  # 5 decimal places
+        if "." not in encoded:
+            encoded += "."
+
+    elif isinstance(value, int):
+        encoded = value
+
+    else:
+        if not isinstance(value, str):
+            value = str(value)
+
         # Best way to encode a string, since it's not exactly documented escape
         # characters properly for liquidsoap.
         encoded = base64.b64encode(value.encode("utf-8")).decode("utf-8")
         encoded = f'base64.decode("{encoded}")'
         if comment_string:
             encoded += f"  # {value!r}"
-    elif isinstance(value, bool):
-        encoded = str(value).lower()
-    elif isinstance(value, float):
-        encoded = f"{value:.5g}"  # 5 decimal places
-        if "." not in encoded:
-            encoded += "."
-    elif isinstance(value, int):
-        encoded = value
-    else:
-        raise ValueError("Invalid value to encode for liquidsoap")
+
     return encoded
